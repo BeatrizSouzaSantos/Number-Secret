bdc97ba2b28c28e70a061b8f41057ce1c117ec86
Beatriz Souza <beatrizsouzasantos1288@gmail.com>
2024-01-25 17:01:07 -0300

    adicionando alerta de erro e acerto

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,11 +1,32 @@
 //querySelector seleciona uma linha de código
 //innerHTML modifica algo que esta dentro 
-let titulo = document .querySelector('h1');
-titulo.innerHTML =  'Jogo do Numero Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
+
+function showText(tag, texto){
+    let campo = document.querySelector(tag);
+     campo.innerHTML = texto;
+
+}
+
+showText('h1', 'Jogo do Numero Secreto');
+showText('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('O botão foi clicado!')
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto){
+        showText('h1', 'Parabéns');
+        showText('p', 'Você descobriu o numero secreto!!')
+    } else {
+        if (chute > numeroSecreto){
+            showText('p', 'O número secreto é menor');
+        } else {
+            showText('p', 'O número secreto é maior');
+
+        }
+    }
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);  
 }
